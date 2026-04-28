8e780f6c22a1b926a0837834ccb166e70789359c
Khushwant Khatri <khatri.khush1998@gmail.com>
2026-04-28 11:27:33 +0530

    my next js app first commit

---
+++ lib/constants.ts
@@ -0,0 +1,67 @@
+export type EventItem = {
+  title: string;
+  image: string;
+  slug: string;
+  location: string;
+  date: string;
+  time: string;
+};
+
+export const events: EventItem[] = [
+  {
+    title: "React Summit US 2026",
+    image: "/images/event1.png",
+    slug: "react-summit-us-2026",
+    location: "San Fransisco",
+    date: "2026-08-01",
+    time: "09:00 AM",
+  },
+  {
+    title: "React Typescript 2026",
+    image: "/images/event2.png",
+    slug: "react-typescript-2026",
+    location: "Austria",
+    date: "2026-08-02",
+    time: "09:00 AM",
+  },
+  {
+    title: "React 2026",
+    image: "/images/event3.png",
+    slug: "react-2026",
+    location: "London",
+    date: "2026-08-03",
+    time: "09:00 AM",
+  },
+  {
+    title: "Open Source Summit North America 2026",
+    image: "/images/event4.png",
+    slug: "oss-na-2026",
+    location: "North America",
+    date: "2026-08-04",
+    time: "09:00 AM",
+  },
+  {
+    title: "ETH Global Paris 2026",
+    image: "/images/event5.png",
+    slug: "eth-global-paris-2026",
+    location: "Paris",
+    date: "2026-08-05",
+    time: "09:00 AM",
+  },
+  {
+    title: "Google Cloud Next 2026",
+    image: "/images/event6.png",
+    slug: "google-cloud-next-2026",
+    location: "San Jose, CA,USA",
+    date: "2026-08-05",
+    time: "09:00 AM",
+  },
+  {
+    title: "Open Source Las Vegas 2026",
+    image: "/images/event-full.png",
+    slug: "os-lv-2026",
+    location: "Las Vegas",
+    date: "2026-08-05",
+    time: "09:00 AM",
+  },
+];
